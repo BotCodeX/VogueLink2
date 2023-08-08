db00dd02c7c56ff819b45fe4a0ee185d8ad5417d
BotCodeX <78162544+BotCodeX@users.noreply.github.com>
2023-08-08 21:45:04 +0600

    Product Updated

--- VogueLink2/Controllers/HomeController.cs
+++ VogueLink2/Controllers/HomeController.cs
@@ -35,10 +35,79 @@ namespace VogueLink2.Controllers
 
             return View();
         }
+
         public ActionResult AllProduct()
         {
+
             return View(db.Products.ToList());
         }
+
+        public ActionResult Search(string category, string searchText)
+        {
+            if(category ==null)
+            {
+                ViewBag.notify = "Select a category";
+                return View("AllProduct", db.Products.ToList());
+            }
+            else if(searchText==null)
+            {
+                ViewBag.notify = "Insert something";
+                return View("AllProduct", db.Products.ToList());
+            }
+            else if (category == "name")
+            {
+                var data = db.Products.SqlQuery("SELECT * FROM Product WHERE Product_Name LIKE '%' + @p0 + '%'", searchText).ToList();
+                if (data != null)
+                {
+                    return View("AllProduct", data);
+                }
+            }
+            else if (category == "brand")
+            {
+                var data = db.Products.SqlQuery("SELECT * FROM Product WHERE Product_Brand LIKE '%' + @p0 + '%'", searchText).ToList();
+                if (data != null)
+                {
+                    return View("AllProduct", data);
+                }
+            }
+            else if (category == "type")
+            {
+                var data = db.Products.SqlQuery("SELECT * FROM Product WHERE Product_Type LIKE '%' + @p0 + '%'", searchText).ToList();
+                if (data != null)
+                {
+                    return View("AllProduct", data);
+                }
+            }
+            else if (category == "fabric")
+            {
+                var data = db.Products.SqlQuery("SELECT * FROM Product WHERE Product_Material LIKE '%' + @p0 + '%'", searchText).ToList();
+                if (data != null)
+                {
+                    return View("AllProduct", data);
+                }
+            }
+            return View("AllProduct");
+        }
+
+        
+
+        public ActionResult Filter(string lowPrice, string highPrice)
+        {
+            int minPrice = Convert.ToInt32(lowPrice);
+            int maxPrice = Convert.ToInt32(highPrice);
+
+            var data = db.Products.Where(p => p.Product_Price >= minPrice && p.Product_Price <= maxPrice).ToList();
+
+            /*
+            // write string gender in parameter
+            if (!string.IsNullOrEmpty(gender))
+            {
+                filteredProducts = filteredProducts.Where(p => p.Gender == gender);
+            }
+            */
+            return View("AllProduct",data);
+        }
+
 
         public ActionResult ProductDetails(int id)
         {
